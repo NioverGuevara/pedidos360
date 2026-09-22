e2e756fc2a30eb88d80e88c18006c69dcd4e1e3d
Alexanderojas8 <al.rojaschacon@duocuc.cl>
2026-09-22 00:18:33 -0300

    feat: integrar frontend Angular con Microsoft Entra ID y MSAL

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { MsalGuard } from '@azure/msal-angular';
 
-export const routes: Routes = [];
+import { Inicio } from './pages/inicio/inicio';
+import { Pedidos } from './pages/pedidos/pedidos';
+import { Redirect } from './redirect/redirect';
+
+export const routes: Routes = [
+  {
+    path: 'redirect',
+    component: Redirect
+  },
+  {
+    path: '',
+    component: Inicio
+  },
+  {
+    path: 'pedidos',
+    component: Pedidos,
+    canActivate: [MsalGuard]
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
